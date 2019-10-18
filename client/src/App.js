b5bdc6865e39892c0a81cf5e6007361b0fc438fe
Omar Salka <omarsalka9@gmail.com>
2019-10-17 22:52:57 -0500

    tic tac toe ui

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './MediaTablet.css';
 import './MediaMobile.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/layout/Home';
-import Tic_Tac_Toe from './components/games/Tic_tac_toe';
+import Tic_tac_toe from './components/games/Tic_tac_toe';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
@@ -16,7 +16,7 @@ const App = () => {
             <Navbar />
             <Switch>
               <Home exact path='/' component={Home} />
-              <Tic_Tac_Toe exact path='/tic-tac-toe' component={Tic_Tac_Toe} />
+              <Tic_tac_toe exact path='/tic-tac-toe' component={Tic_tac_toe} />
             </Switch>
           </div>
         </div>
--- client/src/components/games/Tic_tac_toe.js
+++ client/src/components/games/Tic_tac_toe.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Tic_tac_toe = () => {
+  const onSubmit = e => {
+    e.preventDefault();
+  };
   return (
     <div id='tic-tac-toe'>
       <div className='btn leave' style={{ padding: '0rem' }}>
@@ -16,19 +19,41 @@ const Tic_tac_toe = () => {
       </div>
       <div className='game-space'>
         <div className='game-chat-layout'>
-          <div className='tic-tac-toe-board'>
-            <div className='box one'>1</div>
-            <div className='box two'>2</div>
-            <div className='box three'>3</div>
-            <div className='box four'>4</div>
-            <div className='box five'>5</div>
-            <div className='box six'>6</div>
-            <div className='box seven'>7</div>
-            <div className='box eight'>8</div>
-            <div className='box nine'>9</div>
+          <div className='game-box'>
+            <div className='score-box'>
+              <p>
+                <span className='player-name'>Player1:</span> 0
+              </p>
+              <p>
+                <span className='player-name'>Player2:</span> 0
+              </p>
+            </div>
+            <div className='tic-tac-toe-board'>
+              <div className='box one'>1</div>
+              <div className='box two'>2</div>
+              <div className='box three'>3</div>
+              <div className='box four'>4</div>
+              <div className='box five'>5</div>
+              <div className='box six'>6</div>
+              <div className='box seven'>7</div>
+              <div className='box eight'>8</div>
+              <div className='box nine'>9</div>
+            </div>
           </div>
           <div className='chat-box'>
-            <h1>Let's chat!</h1>
+            <div className='chat-content'>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum
+                quod voluptate accusamus tempora fugit aliquam odit sint
+                voluptatum a placeat?
+              </p>
+            </div>
+            <form className='message-form' onSubmit={onSubmit}>
+              <textarea name='text-bar' cols='45'></textarea>
+              <div className='send-button' onClick={onSubmit}>
+                <i className='far fa-paper-plane'></i>
+              </div>
+            </form>
           </div>
         </div>
       </div>
