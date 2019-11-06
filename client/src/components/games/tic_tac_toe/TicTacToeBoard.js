eace0c2e1bb33088f54f2b3ded275da1d32ac940
Omar Salka <omarsalka9@gmail.com>
2019-11-05 21:51:27 -0600

    styled Xs and Os

--- client/src/components/games/tic_tac_toe/TicTacToeBoard.js
+++ client/src/components/games/tic_tac_toe/TicTacToeBoard.js
@@ -127,9 +127,17 @@ const TicTacToeBoard = ({
         onClick={users.length === 2 && allowed ? box_1_clicked : null}
       >
         {box1.icon === 'x' ? (
-          <i className='fas fa-times fa-3x'></i>
+          <i
+            className='fas fa-times fa-3x'
+            style={{ color: box1.user === name && 'red' }}
+          ></i>
         ) : (
-          box1.icon === 'o' && <i className='far fa-circle fa-3x'></i>
+          box1.icon === 'o' && (
+            <i
+              className='far fa-circle fa-3x'
+              style={{ color: box1.user === name && 'red' }}
+            ></i>
+          )
         )}
       </div>
       <div
@@ -137,9 +145,17 @@ const TicTacToeBoard = ({
         onClick={users.length === 2 && allowed ? box_2_clicked : null}
       >
         {box2.icon === 'x' ? (
-          <i className='fas fa-times fa-3x'></i>
+          <i
+            className='fas fa-times fa-3x'
+            style={{ color: box2.user === name && 'red' }}
+          ></i>
         ) : (
-          box2.icon === 'o' && <i className='far fa-circle fa-3x'></i>
+          box2.icon === 'o' && (
+            <i
+              className='far fa-circle fa-3x'
+              style={{ color: box2.user === name && 'red' }}
+            ></i>
+          )
         )}
       </div>
       <div
@@ -147,9 +163,17 @@ const TicTacToeBoard = ({
         onClick={users.length === 2 && allowed ? box_3_clicked : null}
       >
         {box3.icon === 'x' ? (
-          <i className='fas fa-times fa-3x'></i>
+          <i
+            className='fas fa-times fa-3x'
+            style={{ color: box3.user === name && 'red' }}
+          ></i>
         ) : (
-          box3.icon === 'o' && <i className='far fa-circle fa-3x'></i>
+          box3.icon === 'o' && (
+            <i
+              className='far fa-circle fa-3x'
+              style={{ color: box3.user === name && 'red' }}
+            ></i>
+          )
         )}
       </div>
       <div
@@ -157,9 +181,17 @@ const TicTacToeBoard = ({
         onClick={users.length === 2 && allowed ? box_4_clicked : null}
       >
         {box4.icon === 'x' ? (
-          <i className='fas fa-times fa-3x'></i>
+          <i
+            className='fas fa-times fa-3x'
+            style={{ color: box4.user === name && 'red' }}
+          ></i>
         ) : (
-          box4.icon === 'o' && <i className='far fa-circle fa-3x'></i>
+          box4.icon === 'o' && (
+            <i
+              className='far fa-circle fa-3x'
+              style={{ color: box4.user === name && 'red' }}
+            ></i>
+          )
         )}
       </div>
       <div
@@ -167,9 +199,17 @@ const TicTacToeBoard = ({
         onClick={users.length === 2 && allowed ? box_5_clicked : null}
       >
         {box5.icon === 'x' ? (
-          <i className='fas fa-times fa-3x'></i>
+          <i
+            className='fas fa-times fa-3x'
+            style={{ color: box5.user === name && 'red' }}
+          ></i>
         ) : (
-          box5.icon === 'o' && <i className='far fa-circle fa-3x'></i>
+          box5.icon === 'o' && (
+            <i
+              className='far fa-circle fa-3x'
+              style={{ color: box5.user === name && 'red' }}
+            ></i>
+          )
         )}
       </div>
       <div
@@ -177,9 +217,17 @@ const TicTacToeBoard = ({
         onClick={users.length === 2 && allowed ? box_6_clicked : null}
       >
         {box6.icon === 'x' ? (
-          <i className='fas fa-times fa-3x'></i>
+          <i
+            className='fas fa-times fa-3x'
+            style={{ color: box6.user === name && 'red' }}
+          ></i>
         ) : (
-          box6.icon === 'o' && <i className='far fa-circle fa-3x'></i>
+          box6.icon === 'o' && (
+            <i
+              className='far fa-circle fa-3x'
+              style={{ color: box6.user === name && 'red' }}
+            ></i>
+          )
         )}
       </div>
       <div
@@ -187,9 +235,17 @@ const TicTacToeBoard = ({
         onClick={users.length === 2 && allowed ? box_7_clicked : null}
       >
         {box7.icon === 'x' ? (
-          <i className='fas fa-times fa-3x'></i>
+          <i
+            className='fas fa-times fa-3x'
+            style={{ color: box7.user === name && 'red' }}
+          ></i>
         ) : (
-          box7.icon === 'o' && <i className='far fa-circle fa-3x'></i>
+          box7.icon === 'o' && (
+            <i
+              className='far fa-circle fa-3x'
+              style={{ color: box7.user === name && 'red' }}
+            ></i>
+          )
         )}
       </div>
       <div
@@ -197,9 +253,17 @@ const TicTacToeBoard = ({
         onClick={users.length === 2 && allowed ? box_8_clicked : null}
       >
         {box8.icon === 'x' ? (
-          <i className='fas fa-times fa-3x'></i>
+          <i
+            className='fas fa-times fa-3x'
+            style={{ color: box8.user === name && 'red' }}
+          ></i>
         ) : (
-          box8.icon === 'o' && <i className='far fa-circle fa-3x'></i>
+          box8.icon === 'o' && (
+            <i
+              className='far fa-circle fa-3x'
+              style={{ color: box8.user === name && 'red' }}
+            ></i>
+          )
         )}
       </div>
       <div
@@ -207,9 +271,17 @@ const TicTacToeBoard = ({
         onClick={users.length === 2 && allowed ? box_9_clicked : null}
       >
         {box9.icon === 'x' ? (
-          <i className='fas fa-times fa-3x'></i>
+          <i
+            className='fas fa-times fa-3x'
+            style={{ color: box9.user === name && 'red' }}
+          ></i>
         ) : (
-          box9.icon === 'o' && <i className='far fa-circle fa-3x'></i>
+          box9.icon === 'o' && (
+            <i
+              className='far fa-circle fa-3x'
+              style={{ color: box9.user === name && 'red' }}
+            ></i>
+          )
         )}
       </div>
     </div>
