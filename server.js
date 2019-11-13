98b796bab11e6489d12d93aee14b90a647a53eaa
Omar Salka <omarsalka9@gmail.com>
2019-11-12 22:03:49 -0600

    fixing socket not communicating with ssl

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const path = require('path');
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const uuid = require('uuid');
+const https = require("https").createServer(app);
+const io = require("socket.io")(https);
+const uuid = require("uuid");
 
-const socket = require('./socket');
+const socket = require("./socket");
 
-const userMethods = require('./users');
-const ttt_boardMethods = require('./ticTacToeBoard');
+const userMethods = require("./users");
+const ttt_boardMethods = require("./ticTacToeBoard");
 
 // Connect to database
 connectDB();
@@ -19,14 +19,14 @@ socket(io, uuid, userMethods, ttt_boardMethods);
 // Serve static assets in production
 // if (process.env.NODE_ENV === 'production') {
 // Set static folder
-app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+app.use(express.static(path.resolve(__dirname, "client", "build")));
 // app.use(express.static('client/build'));
 
-app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
 );
 // }
 
 const PORT = process.env.PORT || 5000;
 
-http.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+https.listen(PORT, () => console.log(`Server started on port ${PORT}`));
