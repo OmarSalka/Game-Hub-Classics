b38092b589eaddc2fd18d512d002836ced915a71
Omar Salka <omarsalka9@gmail.com>
2019-11-12 21:03:32 -0600

    making socket.io connection secure

--- client/src/actions/socketRoutingActions.js
+++ client/src/actions/socketRoutingActions.js
@@ -5,33 +5,34 @@ import {
   DISCONNECT,
   DISPLAY_WELCOME_MESSAGE,
   GET_ROOM_DATA
-} from './types';
-import { errorLogger } from './alertActions';
-import { delete_chat } from './chatActions';
-import { clear_board } from './ticTacToeActions';
+} from "./types";
+import { errorLogger } from "./alertActions";
+import { delete_chat } from "./chatActions";
+import { clear_board } from "./ticTacToeActions";
 
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
 let socket;
 const ENDPOINT = `:${process.env.PORT || 5000}`;
 
 export const join = (joinData, game) => dispatch => {
-  socket = io(ENDPOINT, {secure: true});
+  socket = io("https://gamehubclassics.com", { secure: true });
+  // socket = io(ENDPOINT, {secure: true});
   const { name, room } = joinData;
 
   // join
-  socket.emit('join', { name, room }, error => {
+  socket.emit("join", { name, room }, error => {
     if (error) {
-      dispatch(errorLogger({ type: 'danger', msg: error }));
+      dispatch(errorLogger({ type: "danger", msg: error }));
     } else {
-      game === 'tic_tac_toe'
+      game === "tic_tac_toe"
         ? dispatch({ type: T3_JOIN_SUCCESS })
-        : game === 'dots_boxes' && dispatch({ type: DB_JOIN_SUCCESS });
+        : game === "dots_boxes" && dispatch({ type: DB_JOIN_SUCCESS });
     }
   });
 
   // send welcome message
-  socket.once('message', message => {
+  socket.once("message", message => {
     const { id, user, text } = message;
     dispatch({
       type: DISPLAY_WELCOME_MESSAGE,
@@ -49,7 +50,7 @@ export const join = (joinData, game) => dispatch => {
 
 // get online users
 export const getOnlineUsers = name => dispatch => {
-  socket.on('room data', roomData => {
+  socket.on("room data", roomData => {
     const { users } = roomData;
     const existingOponent = users.find(user => user.name !== name);
     const icon = users.find(user => user.name === name).icon;
@@ -64,7 +65,7 @@ export const getOnlineUsers = name => dispatch => {
 
 // display message
 export const display_message = () => dispatch => {
-  socket.on('message', message => {
+  socket.on("message", message => {
     const { id, user, text } = message;
     dispatch({
       type: DISPLAY_WELCOME_MESSAGE,
@@ -75,7 +76,7 @@ export const display_message = () => dispatch => {
 
 // send message
 export const send_message = textMessage => {
-  socket.emit('send message', textMessage);
+  socket.emit("send message", textMessage);
 };
 
 // disconnect
